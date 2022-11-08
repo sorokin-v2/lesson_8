35a5bd7e4af2d8452763e13c361261c457615b51
Valery Sorokin <podrabotka-2019@yandex.ru>
2022-11-08 16:00:57 +0300

    first commit

---
+++ task_2/task_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class node
+{
+public:
+    int m_value;
+    //std::shared_ptr<node> parent;     //Для устранения циклической зависимости вместо shared_ptr используем weak_ptr
+    std::weak_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+};
+
+int main()
+{
+    {
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+        node1->parent = node2;              //Формируется циклическая
+        node2->parent = node1;              //зависимость, по-этому при выходе из блока объекты не удаляются(память утекает)
+    }
+
+    return 0;
+}
---
+++ task_3/task_3.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+template <typename T>
+class My_unique_ptr {
+public:
+    
+    My_unique_ptr(): ptr_(nullptr) {}                 //ptr_ инициализируется nullptr ( T* ptr_{ nullptr };), по-этому этот конструктор не обязателен?
+    // Explicit constructor
+    explicit My_unique_ptr(T* src) : ptr_(src) {}       //Указатель сознательно не константный
+    // Constructor/Assignment that binds to nullptr
+    // This makes usage with nullptr cleaner
+    My_unique_ptr(std::nullptr_t) : ptr_(nullptr) {}  //ptr_ инициализируется nullptr ( T* ptr_{ nullptr };), по-этому этот конструктор не обязателен?
+    
+    My_unique_ptr(My_unique_ptr&& moving) noexcept : ptr_(nullptr)
+    {
+        moving.swap(*this);
+        // In the comments it was pointed out that this
+        // does not match the implementation of std::unique_ptr
+        // I am going to leave mine the same. But
+        // the the standard provides some extra guarantees
+        // and probably a more intuitive usage.
+    }
+    
+    ~My_unique_ptr() {
+        delete ptr_;
+    }
+
+    My_unique_ptr& operator= (std::nullptr_t)
+    {
+        reset();
+        return *this;
+    }
+
+    My_unique_ptr& operator= (My_unique_ptr&& moving) noexcept
+    {
+        moving.swap(*this);
+        return *this;
+        // See move constructor.
+    }
+
+    T& operator*() const { return *ptr_;}
+    T* operator->() const { return ptr_; }
+    explicit operator bool() const { return ptr_; }
+    
+    T* release() noexcept
+    {
+        T* result = nullptr;
+        std::swap(result, ptr_);
+        return result;
+    }
+  
+    void reset() {
+        T* tmp = release();
+        delete tmp;
+    }
+
+    void swap(My_unique_ptr& src) noexcept
+    {
+        std::swap(ptr_, src.ptr);
+    }
+
+
+    //Запрещаем конструктор копирования и operator=
+    My_unique_ptr(const My_unique_ptr&) = delete;
+    My_unique_ptr& operator=(const My_unique_ptr&) = delete;
+
+private:
+    T* ptr_{ nullptr };
+
+};
+
+int main()
+{
+    My_unique_ptr<int> my_unique_ptr (new int(100));
+    std::cout << *my_unique_ptr << "\n";
+    int* val2 = my_unique_ptr.release();
+    std::cout << *val2 << "\n";
+}
